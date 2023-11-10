8632af9749798a318b06b699d6af37747a24ae75
be-taozhu <104804519+be-taozhu@users.noreply.github.com>
2023-11-10 21:49:47 +0800

    test

---
+++ BasicMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BasicMVCProject.Startup))]
+namespace BasicMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
